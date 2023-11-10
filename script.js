5a504a09f0dbe4fdc342349e809a00990961c6ea
matheusgomespecin <134521816+matheusgomespecin@users.noreply.github.com>
2023-11-10 08:56:58 -0300

    alerta

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
+
+
+function minhaFuncaoExistente() {
+    console.log('Esta é uma função existente!');
+  }
+  
+  function novaFuncaoDeAlerta() {
+    alert('Botão clicado! Exibindo um alerta na tela.');
+  }
+  
+  document.getElementById('seuBotaoID').addEventListener('click', novaFuncaoDeAlerta);
+  
